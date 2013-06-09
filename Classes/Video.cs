8ae940f3d1d3dc37cc415678e0bd939378d34061
hugues <hugues@f4bf9ab9-1d2c-fe40-97ed-177ebaf4b57a>
2013-06-09 16:51:58 +0000

    VideoPlayer added
    git-svn-id: svn://zotac/home/svn/VideoPlayer/trunk@67 f4bf9ab9-1d2c-fe40-97ed-177ebaf4b57a

---
+++ Classes/Video.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace VideoPlayer
+{
+    public class Video
+    {
+        [XmlAttribute("FileName")]
+        public String  FileName { get; set; }
+
+        [XmlAttribute("Title")]
+        public String Title { get; set; }
+
+        [XmlIgnore]
+        public TimeSpan Length { get; set; }
+
+        [XmlAttribute("Length")]
+        public String LengthString
+        {
+            get { return this.Length.ToString(); }
+            set { value = this.Length.ToString("HHmmss"); }
+        }
+
+        [XmlArray("Tags")]
+        [XmlArrayItem("Tag")]
+        public List<String> Tags { get; set; }
+
+        public String Directory { get; set; }
+        public String Preview { get; set; }
+    }
+}
---
+++ VideoPlayer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace VideoPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Video video = new Video();
+            video.Directory = "directory";
+            video.FileName = "filename";
+            video.Length = new TimeSpan(0, 10, 30);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Video));
+            using (StreamWriter wr = new StreamWriter("library.xml"))
+            {
+                xmlSerializer.Serialize(wr, video);
+            }
+
+        }
+    }
+}

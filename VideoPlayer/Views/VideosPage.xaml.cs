edc0b9fe89a824717e75ea4e457df7ccece97731
hugues <hugues@f4bf9ab9-1d2c-fe40-97ed-177ebaf4b57a>
2014-12-06 17:02:08 +0000

    git-svn-id: svn://zotac/home/svn/VideoPlayer/trunk@150 f4bf9ab9-1d2c-fe40-97ed-177ebaf4b57a

--- VideoPlayer/Views/VideosPage.xaml.cs
+++ VideoPlayer/Views/VideosPage.xaml.cs
@@ -3,7 +3,6 @@
 //using Vlc.DotNet.Wpf;
 
 using System;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -36,16 +35,7 @@ namespace VideoPlayer
 
         #endregion
 
-        private void Player_Stopped(object sender, EventArgs e)
-        {
-            this.StopVideoPlaying();
-        }
-        
-        private void _uiFilterButton_Click(Object sender, RoutedEventArgs e)
-        {
-            ICollectionView view = this._uiFilesListBox.Items;
-            view.Filter = this.Filter;
-        }
+        #region Filters
 
         private Boolean Filter(Object item)
         {
@@ -83,7 +73,7 @@ namespace VideoPlayer
             }
             return isCategoryOk;
         }
-        
+
         private Boolean IsTagOk(Video video)
         {
             Boolean isTagOk = false;
@@ -110,25 +100,29 @@ namespace VideoPlayer
             return isTagOk;
         }
 
+        private Boolean TrueFilter(Object item)
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region Events
+
+        private void Player_Stopped(object sender, EventArgs e)
+        {
+            this.StopVideoPlaying();
+        }
+
+        private void _uiFilterButton_Click(Object sender, RoutedEventArgs e)
+        {
+            ICollectionView view = this._uiFilesListBox.Items;
+            view.Filter = this.Filter;
+        }
+
         private void _uiPlayAll_OnClick(Object sender, RoutedEventArgs e)
         {
             this.PlayAll();
-            //foreach (Object item in this._uiFilesListBox.Items)
-            //{
-            //    this.Player.AddVideo((Video)item);
-            //    //this.AddToExistingPlaylist(item as Video);
-            //}
-            //this.Player.PlayAll();
-            //this.PlaySelectedVideo(false);
-        }
-
-        private void PlayAll()
-        {
-            foreach (Object item in this._videosTabControlViewModel.FilteredVideos)
-            {
-                this.Player.AddVideo((Video) item);
-            }
-            this.Player.PlayAll();
         }
 
         private void _uiVideoPlaying_KeyUp(Object sender, KeyEventArgs e)
@@ -146,22 +140,10 @@ namespace VideoPlayer
             view.Filter = this.TrueFilter;
         }
 
-        private Boolean TrueFilter(Object item)
-        {
-            return true;
-        }
-
         private void UiPlayAllButton_OnClick(object sender, RoutedEventArgs e)
         {
             this.PlayAll();
-            //foreach (Object item in this._uiFilesListBox.Items)
-            //{
-            //    this.AddToExistingPlaylist(item as Video);
-            //}
-            //this.PlaySelectedVideo(false);
         }
-
-        #region Events
 
         private void ListBoxItem_MouseDoubleClick(Object sender, MouseButtonEventArgs e)
         {
@@ -187,8 +169,6 @@ namespace VideoPlayer
                 else if (e.Key == Key.Return)
                 {
                     this.PlayCurrent();
-                    //this.SwitchToFullScreen();
-                    //this.Player.PlayVideo(this._videosTabControlViewModel.CurrentVideo);
                     e.Handled = true;
                 }
                 else if (e.Key == Key.A)
@@ -200,35 +180,8 @@ namespace VideoPlayer
                 {
                     this.SwitchToFullScreen();
                     this.Player.PlayAll();
-                    //this._isPlaylist = true;
-                    //this.PlaySelectedVideo(false);
-                    //this.playlistPosition = 0;
                     e.Handled = true;
                 }
-                    //else if (e.Key == Key.C)
-                    //{
-                    //    if (this._uiPlaylist.IsVisible)
-                    //    {
-                    //        this._uiPlaylist.Visibility = Visibility.Collapsed;
-                    //    }
-                    //    else
-                    //    {
-                    //        this._uiPlaylist.Visibility = Visibility.Visible;
-                    //    }
-                    //    e.Handled = true;
-                    //}
-                    //else if (e.Key == Key.Space)
-                    //{
-                    //    if (this._VLCcontrol.IsPaused)
-                    //    {
-                    //        this._VLCcontrol.Play();
-                    //    }
-                    //    else
-                    //    {
-                    //        this._VLCcontrol.Pause();
-                    //    }
-                    //    e.Handled = true;
-                    //}
                 else if (e.Key == Key.E)
                 {
                     if (Keyboard.Modifiers == ModifierKeys.Control)
@@ -241,13 +194,6 @@ namespace VideoPlayer
                         }
                         this._uiVideoInfoTabControl.SelectedIndex = 0;
                         this._uiFilesListBox.Items.Refresh();
-                        //this._uiFilesListBox.Items.SortDescriptions.Clear();
-                        //this._uiFilesListBox.Items.SortDescriptions.Add(
-                        //    new SortDescription(this._uiSortComboBox.SelectedItem.ToString(),
-                        //        ListSortDirection.Ascending));
-                        //this._uiFilesListBox.Items.SortDescriptions.Add(
-                        //    new SortDescription("Title",
-                        //        ListSortDirection.Ascending));
                     }
                     else
                     {
@@ -258,14 +204,23 @@ namespace VideoPlayer
             }
         }
 
+        #endregion
+
+        #region Methods
+
+        private void PlayAll()
+        {
+            foreach (Object item in this._videosTabControlViewModel.FilteredVideos)
+            {
+                this.Player.AddVideo((Video) item);
+            }
+            this.Player.PlayAll();
+        }
+
         private void AddToExistingPlaylist()
         {
             this.Player.AddVideo(this._videosTabControlViewModel.CurrentVideo);
         }
-        
-        #endregion
-
-        #region Methods
 
         private void PlayCurrent()
         {
@@ -278,13 +233,11 @@ namespace VideoPlayer
             else
             {
                 MessageBox.Show("File not found");
-                //this.Logger().ErrorFormat("Could not find file {0}", video.FileName);
             }
         }
 
         private void StopVideoPlaying()
         {
-            //this._uiNowPlaying.Text = "Now playing: ";
             this.SwitchToWindowMode();
         }
 

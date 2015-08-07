655843c8992c6ebb9b616b1094562c1a2ca28e3e
hugues <hugues@f4bf9ab9-1d2c-fe40-97ed-177ebaf4b57a>
2015-08-07 09:44:38 +0000

    HomeModule refactroring to use ModuleBase
    git-svn-id: svn://zotac/home/svn/VideoPlayer/trunk@188 f4bf9ab9-1d2c-fe40-97ed-177ebaf4b57a

--- HomeModule/HomeModule.cs
+++ HomeModule/HomeModule.cs
@@ -1,34 +1,21 @@
-﻿using Microsoft.Practices.Prism.Modularity;
-using Microsoft.Practices.Prism.Regions;
+﻿using Microsoft.Practices.Prism.Regions;
 using Microsoft.Practices.Unity;
 using VideoPlayer.Infrastructure;
 
 namespace HomeModule
 {
-    public class HomeModule : IModule
+    public class HomeModule : ModuleBase
     {
-        private readonly IUnityContainer _unityContainer;
-        private readonly IRegionManager _regionManager;
 
-        public HomeModule(IUnityContainer unityContainer, IRegionManager regionManager) //: base(unityContainer, regionManager)
+        public HomeModule(IUnityContainer unityContainer, IRegionManager regionManager) : base(unityContainer, regionManager)
         {
-            this._unityContainer = unityContainer;
-            this._regionManager = regionManager;
         }
 
-        public void Initialize()
+        public override void Initialize()
         {
-
-            this._unityContainer.RegisterType<IHomePageViewModel, HomePageViewModel>();
-            this._unityContainer.RegisterType<IHomePage, HomePage>();
-
-
-            if (this._regionManager.Regions.ContainsRegionWithName(RegionNames.HomeRegion))
-            {
-                IRegion region = this._regionManager.Regions[RegionNames.HomeRegion];
-                var viewModel = this._unityContainer.Resolve<IHomePageViewModel>();
-                region.Add(viewModel.View);
-            }
+            this.RegisterType<IHomePageViewModel, HomePageViewModel>();
+            this.RegisterType<IHomePage, HomePage>();
+            this.ReferenceRegion<IHomePageViewModel>(RegionNames.HomeRegion);
         }
     }
 }

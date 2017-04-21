c88d301010202cde1598f77aca48d0db9cb73aa3
Hugues Stefanski <hugues.stefanski@gmail.com>
2017-04-21 12:34:15 +0200

    Cleaned code

--- Modules/PlaylistModule/PlayListModule.cs
+++ Modules/PlaylistModule/PlayListModule.cs
@@ -6,12 +6,10 @@ namespace PlaylistModule
 {
     public class PlayListModule : ModuleBase
     {
-        private readonly IUnityContainer _container;
         private readonly IRegionManager _regionManager;
 
         public PlayListModule(IUnityContainer container, IRegionManager regionManager) : base(container, regionManager)
         {
-            this._container = container;
             this._regionManager = regionManager;
         }
 
@@ -19,20 +17,7 @@ namespace PlaylistModule
         {
             this.RegisterType<IPlayListViewModel, PlayListViewModel>();
             this.RegisterType<IPlayListView, PlayListView>();
-            var regionName = RegionNames.PlayListRegion;
-
-                this._regionManager.RegisterViewWithRegion(regionName,typeof(IPlayListView));
-            //var view = this._container.Resolve<IPlayListView>();
-            //if (this._regionManager.Regions.ContainsRegionWithName(regionName))
-            //{
-            //    var region = this._regionManager.Regions[regionName];
-            //    region.Add(view);
-            //}
-            //else
-            //{
-            //    this._regionManager.RegisterViewWithRegion(regionName, view.GetType);
-            //}
-            //this.ReferenceRegion<IPlayListViewModel>(RegionNames.PlayListRegion);
+            this._regionManager.RegisterViewWithRegion(RegionNames.PlayListRegion, typeof (IPlayListView));
         }
     }
 }

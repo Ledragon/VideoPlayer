6790ad1384bd97488f542de647ce8bb0bad00ea2
Hugues Stefanski <hugues.stefanski@gmail.com>
2018-06-23 12:01:52 +0200

    Code format

--- Log/Logger.cs
+++ Log/Logger.cs
@@ -14,52 +14,82 @@ namespace Log
 
         public void Debug(Object message)
         {
-            if (this._log.IsDebugEnabled) this._log.Debug(message);
+            if (this._log.IsDebugEnabled)
+            {
+                this._log.Debug(message);
+            }
         }
 
         public void DebugFormat(String format, params Object[] args)
         {
-            if (this._log.IsDebugEnabled) this._log.DebugFormat(format, args);
+            if (this._log.IsDebugEnabled)
+            {
+                this._log.DebugFormat(format, args);
+            }
         }
 
         public void Info(Object message)
         {
-            if (this._log.IsInfoEnabled) this._log.Info(message);
+            if (this._log.IsInfoEnabled)
+            {
+                this._log.Info(message);
+            }
         }
 
         public void InfoFormat(String format, params Object[] args)
         {
-            if (this._log.IsInfoEnabled) this._log.InfoFormat(format, args);
+            if (this._log.IsInfoEnabled)
+            {
+                this._log.InfoFormat(format, args);
+            }
         }
 
         public void Warn(Object message)
         {
-            if (this._log.IsWarnEnabled) this._log.Warn(message);
+            if (this._log.IsWarnEnabled)
+            {
+                this._log.Warn(message);
+            }
         }
 
         public void WarnFormat(String format, params Object[] args)
         {
-            if (this._log.IsWarnEnabled) this._log.WarnFormat(format, args);
+            if (this._log.IsWarnEnabled)
+            {
+                this._log.WarnFormat(format, args);
+            }
         }
 
         public void Error(Object message)
         {
-            if (this._log.IsErrorEnabled) this._log.Error(message);
+            if (this._log.IsErrorEnabled)
+            {
+                this._log.Error(message);
+            }
         }
 
         public void ErrorFormat(String format, params Object[] args)
         {
-            if (this._log.IsErrorEnabled) this._log.ErrorFormat(format, args);
+            if (this._log.IsErrorEnabled)
+            {
+                this._log.ErrorFormat(format, args);
+            }
         }
 
         public void Fatal(Object message)
         {
-            if (this._log.IsFatalEnabled) this._log.Fatal(message);
+            if (this._log.IsFatalEnabled)
+            {
+                this._log.Fatal(message);
+            }
         }
 
         public void FatalFormat(String format, params Object[] args)
         {
-            if (this._log.IsFatalEnabled) this._log.FatalFormat(format, args);
+            if (this._log.IsFatalEnabled)
+            {
+                this._log.FatalFormat(format, args);
+            }
         }
     }
 }

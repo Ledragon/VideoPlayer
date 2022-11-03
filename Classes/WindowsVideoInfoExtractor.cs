dc26aa3593d3020b7343c4922cf1aa9145428902
Hugues Stefanski <hugues.stefanski@gmail.com>
2022-11-03 15:55:00 +0100

    Tried minor improvement

--- Classes/WindowsVideoInfoExtractor.cs
+++ Classes/WindowsVideoInfoExtractor.cs
@@ -9,15 +9,20 @@ namespace Classes
         {
             using (var shellFile = ShellFile.FromFilePath(video.FileName))
             {
-                var thumbnail = shellFile.Thumbnail.ExtraLargeBitmap;
-                video.PreviewImage = thumbnail;
-                var duration = shellFile.Properties.System.Media.Duration.Value;
-
-                Double nanoSeconds = 0;
-                if (Double.TryParse(duration.ToString(), out nanoSeconds))
+                if (video.PreviewImage == null)
                 {
-                    var milliSeconds = nanoSeconds * 0.0001;
-                    video.Length = TimeSpan.FromMilliseconds(milliSeconds);
+                    var thumbnail = shellFile.Thumbnail.ExtraLargeBitmap;
+                    video.PreviewImage = thumbnail;
+                }
+                if (video.Length == TimeSpan.Zero)
+                {
+                    var duration = shellFile.Properties.System.Media.Duration.Value;
+                    Double nanoSeconds = 0;
+                    if (Double.TryParse(duration.ToString(), out nanoSeconds))
+                    {
+                        var milliSeconds = nanoSeconds * 0.0001;
+                        video.Length = TimeSpan.FromMilliseconds(milliSeconds);
+                    }
                 }
                 var rating = shellFile.Properties.System.Rating.Value;
                 UInt32 myRating = 0;

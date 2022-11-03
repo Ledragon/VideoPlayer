b932e1a510690aab8ed3ddc1646dc1ad1657d73d
Hugues Stefanski <hugues.stefanski@gmail.com>
2022-11-03 15:52:27 +0100

    Removed unnecessary using

--- Classes/WindowsVideoInfoExtractor.cs
+++ Classes/WindowsVideoInfoExtractor.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.WindowsAPICodePack.Shell;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Classes
 {

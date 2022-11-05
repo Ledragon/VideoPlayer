cc97ac25229a3ca915d93cb733d24a925fc797f2
Hugues Stefanski <hugues.stefanski@gmail.com>
2022-11-05 10:26:59 +0100

    Working controller

---
+++ VideoPlayer.WebAPI/Controllers/VideosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoPlayer.Entities;
+
+namespace VideoPlayer.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/videos")]
+    public class VideosController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetVideos()
+        {
+            return new JsonResult(new List<Video>
+            {
+                new Video{ Title="Harry Potter"},
+                new Video{ Title = "Lord of the rings"},
+                new Video{ Title  ="Dead Sushi"},
+            });
+        }
+    }
+}

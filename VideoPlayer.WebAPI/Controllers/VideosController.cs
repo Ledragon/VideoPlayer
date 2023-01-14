2d09da6965b1c89c7864a1e32ccb5f30752ffee1
le_dragon <hugues.stefanski@gmail.com>
2023-01-14 18:14:30 +0100

    Genrate fake metadata Deleted old residual project

--- VideoPlayer.WebAPI/Controllers/VideosController.cs
+++ VideoPlayer.WebAPI/Controllers/VideosController.cs
@@ -20,14 +20,29 @@ namespace VideoPlayer.WebAPI.Controllers
         [HttpGet]
         public List<Video> GetVideos()
         {
-            var filePath = @"E:\Hugues Stefanski\Documents\Development\GitHub\VideoPlayer\VideoPlayer\bin\Debug\Files\Library.xml";
-            return this._videoRepository.Load(filePath).Videos;
+            var filePath = @"/home/hugues/Documents/Code/files/Library.xml";
+            var videos = this._videoRepository.Load(filePath).Videos;
+            return videos;
             //return new JsonResult(new List<Video>
             //{
             //    new Video{ Title="Harry Potter"},
             //    new Video{ Title = "Lord of the rings"},
             //    new Video{ Title  ="Dead Sushi"},
             //});
+        }
+
+        [HttpGet("/api/videos/metadata")]
+        public Dictionary<String, VideoMetaData> GetMetaData()
+        {
+            var filePath = @"/home/hugues/Documents/Code/files/Library.xml";
+            var videos = this._videoRepository.Load(filePath).Videos;
+            return videos.ToDictionary(d => d.FileName, d => new VideoMetaData { Codec = "", HasContactSheet = System.IO.File.Exists(d.FileName + ".png") });
+        }
+
+        [HttpPost("/api/videos/previews")]
+        public void GeneratePreviews([FromRoute] String path, [FromRoute] Int32 count)
+        {
+
         }
     }
 }
---
+++ VideoPlayer.WebAPI/Models/VideoMetaData.cs
@@ -0,0 +1,5 @@
+public class VideoMetaData
+{
+  public String Codec { get; set; }
+  public Boolean HasContactSheet { get; set; }
+}

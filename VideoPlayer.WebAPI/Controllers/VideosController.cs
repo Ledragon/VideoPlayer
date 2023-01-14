5d1fb506deeb64cb5285bc4a5669affce10ca0e2
Hugues Stefanski <hugues.stefanski@gmail.com>
2023-01-14 14:13:27 +0100

    Actual loading of file

--- VideoPlayer.WebAPI/Controllers/VideosController.cs
+++ VideoPlayer.WebAPI/Controllers/VideosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VideoPlayer.Database.Repository;
 using VideoPlayer.Entities;
 
 namespace VideoPlayer.WebAPI.Controllers
@@ -8,21 +9,25 @@ namespace VideoPlayer.WebAPI.Controllers
     public class VideosController : ControllerBase
     {
         private readonly ILogger<VideosController> _logger;
+        private readonly IVideoRepository _videoRepository;
 
-        public VideosController(ILogger<VideosController> logger)
+        public VideosController(ILogger<VideosController> logger, IVideoRepository videoRepository)
         {
             this._logger = logger;
+            this._videoRepository = videoRepository;
         }
 
         [HttpGet]
-        public JsonResult GetVideos()
+        public List<Video> GetVideos()
         {
-            return new JsonResult(new List<Video>
-            {
-                new Video{ Title="Harry Potter"},
-                new Video{ Title = "Lord of the rings"},
-                new Video{ Title  ="Dead Sushi"},
-            });
+            var filePath = @"E:\Hugues Stefanski\Documents\Development\GitHub\VideoPlayer\VideoPlayer\bin\Debug\Files\Library.xml";
+            return this._videoRepository.Load(filePath).Videos;
+            //return new JsonResult(new List<Video>
+            //{
+            //    new Video{ Title="Harry Potter"},
+            //    new Video{ Title = "Lord of the rings"},
+            //    new Video{ Title  ="Dead Sushi"},
+            //});
         }
     }
 }

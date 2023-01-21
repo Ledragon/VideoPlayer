db85f966a8d7a75103f0c74b8ae216a648bfe5a1
Hugues Stefanski <hugues.stefanski@gmail.com>
2023-01-21 15:00:12 +0100

    New interface

---
+++ VideoPlayer.Database.Repository.Contracts/IVideoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using VideoPlayer.Entities;
+
+namespace VideoPlayer.Database.Repository.Contracts
+{
+    public interface IVideoRepository
+    {
+        List<Video> Get();
+        Video Get(Int32 id);
+        Video Get(String filePath);
+        Video Add(Video video);
+        List<Video> Add(List<Video> videos);
+        Video Update(Video video);
+        List<Video> Update(List<Video> video);
+    }
+}

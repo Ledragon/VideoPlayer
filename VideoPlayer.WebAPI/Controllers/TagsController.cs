b26daf3efce6050c7e705eb081ac77e9a31fe7b9
Hugues Stefanski <hugues.stefanski@gmail.com>
2023-04-14 12:34:33 +0200

    Endpoint to add tag

--- VideoPlayer.WebAPI/Controllers/TagsController.cs
+++ VideoPlayer.WebAPI/Controllers/TagsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using VideoPlayer.Database.Repository.Contracts;
 using VideoPlayer.Entities;
 
@@ -20,6 +19,12 @@ namespace VideoPlayer.WebAPI.Controllers
         public List<Tag> Get()
         {
             return this._tagsRepository.Get();
+        }
+
+        [HttpPost]
+        public Tag Add([FromBody] Tag tag)
+        {
+            return this._tagsRepository.Add(tag);
         }
 
     }

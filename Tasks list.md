3a2672a752877b45a49578f5911044bdb89dde9f
Hugues Stefanski <hugues.stefanski@gmail.com>
2023-04-14 15:06:22 +0200

    Changed thumbnail size tasks list

--- Tasks list.md
+++ Tasks list.md
@@ -9,9 +9,12 @@
 [ ] ~~Changer la méthode pour générer les thumbs pour être plus efficace~~ => pas forcément mieux, pas juste, à revoir
 [x] Host react app in API (+API url handling)
 [x] Modifier liste des tags d'une video
-[ ] Convert and store contact sheets
+[x] Convert and store contact sheets
+[x] Reduire la taille des images
+[x] Créer des nouveaux tags
+[ ] Vue master/detail plus pratique
 [ ] Repérer les vidéos qui n'ont pas de thumb/CS
-[ ] Créer des nouveaux tags
+[ ] Configurer un pipeline de build
 [ ] Choix conteneur IoC
 [ ] Stream video en mp4 on-the-fly
 [ ] Regarder les infos et récupérer plus de ffprobe
--- VideoPlayer.Ffmpeg/FfmpegThumbnailGenerator.cs
+++ VideoPlayer.Ffmpeg/FfmpegThumbnailGenerator.cs
@@ -51,7 +51,7 @@ namespace VideoPlayer.Ffmpeg
                         process.StartInfo.ArgumentList.Add("-vf");
                         
                         Double fps = (Double.Parse(info.format.duration, CultureInfo.InvariantCulture) / count);
-                        process.StartInfo.ArgumentList.Add($"fps=1/{fps.ToString(CultureInfo.InvariantCulture)}");
+                        process.StartInfo.ArgumentList.Add($"fps=1/{fps.ToString(CultureInfo.InvariantCulture)},scale=\"640:-1\"");
 
                         process.StartInfo.ArgumentList.Add($"thumb_{Path.GetFileNameWithoutExtension(videoFilePath)}_%03d.png");
                         process.StartInfo.RedirectStandardOutput = false;
@@ -99,7 +99,7 @@ namespace VideoPlayer.Ffmpeg
                         process.StartInfo.ArgumentList.Add("-i");
                         process.StartInfo.ArgumentList.Add(videoFilePath);
                         process.StartInfo.ArgumentList.Add("-vf");
-                        process.StartInfo.ArgumentList.Add($"fps=1/{fps.ToString(CultureInfo.InvariantCulture)},tile={cols}x{rows}");
+                        process.StartInfo.ArgumentList.Add($"fps=1/{fps.ToString(CultureInfo.InvariantCulture)},tile={cols}x{rows},scale=640:-1");
                         process.StartInfo.ArgumentList.Add(fileName);
                         process.StartInfo.RedirectStandardOutput = false;
                         process.StartInfo.UseShellExecute = false;
